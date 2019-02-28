1907d052667097b6b1a51c69ba0cb0ae3d2b9d00
Laura Lancheros <lauralancheros@yahoo.com>
2019-02-28 12:23:47 -0500

    works but table/hemi

---
+++ templates/scratch.py
@@ -0,0 +1,8 @@
+<h3>{{ mars.title[0] }}<h3>
+    <img src="{{mars.hemi_image_url[0]}}" alt="Featured mars image">
+    <h3>{{ mars.title[1] }}<h3>
+    <img src="{{mars.hemi_image_url[1]}}" alt="Featured mars image">
+    <h3>{{ mars.title[2] }}<h3>
+    <img src="{{mars.hemi_image_url[2]}}" alt="Featured mars image">
+    <h3>{{ mars.title[3] }}<h3>
+    <img src="{{mars.hemi_image_url[3]}}" alt="Featured mars image"></div>
